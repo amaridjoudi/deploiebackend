171d29538219202f9882f81d8a6bd042b4de655a
Djoudi amari <amari.djoudii@gmail.com>
2023-01-20 14:58:33 +0100

    try fix

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var router = express.Router();
 
 router.get('/year', function(req, res) {
 
-  const date = new Date();
-  res.json({year: Date.getFullYear()})
+  const date = new Date().getFullYear().toString();
+  res.json({year: date})
 });
 
 module.exports = router;
