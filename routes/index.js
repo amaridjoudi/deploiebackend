1809dcaa01ea2d7c40401aa1d11bcb3076dafdcf
Djoudi amari <amari.djoudii@gmail.com>
2023-01-20 12:14:32 +0100

    gdg

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+
+const lien = 'deploiebackend-pp10q1zut-amaridjoudi.vercel.app'
+
+router.get('/lien', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
